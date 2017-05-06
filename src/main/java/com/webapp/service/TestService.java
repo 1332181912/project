c6d8a22a4911a8b80a818c7ad74ca7620b85e1bb
yong.zhang <1332181912@qq.com>
2017-05-06 15:27:10 +0800

    test remove

---
+++ src/main/java/com/webapp/service/TestService.java
@@ -0,0 +1,11 @@
+package com.webapp.service;
+
+/**
+ * Created by yong.zhang on 2017/5/6.
+ */
+public class TestService {
+    public void test()
+    {
+        System.out.println("~~~~~~~~~~~~~~~~");
+    }
+}
